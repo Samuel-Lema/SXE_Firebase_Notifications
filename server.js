49be54d9558fa48ca331df462309ab05f0e508bd
samuel-lema <ec2-user@ip-172-31-28-222.us-east-2.compute.internal>
2019-03-26 10:31:08 +0000

    Aplicación documentada.

--- server.js
+++ server.js
@@ -25,66 +25,67 @@ var ref = db.ref("/");
 var resultado = null;
 
 //Listener Base de datos que se actualiza en tiempo real
+
 ref.on("value", function(snapshot) {
     
     resultado = snapshot;
     
+    // Variables de uso en las notificaciones
     let usuario = ""
     let msg = ""
     
+    // Recorro todos los mensajes y guardo el último para enviarlo por broadcast a todos
     resultado.forEach(element => {
 
         if (element.key == "mensajes") {
                     
             element.forEach(elementChild => {
 
-                console.log("KEY: " + elementChild.key);
+                // Guardo el usuario que mando el mensaje
                 usuario = elementChild.val().tokenUsuario
-                console.log("VALOR: " + elementChild.val().mensaje);
+                // Guardo el mensaje
                 msg = elementChild.val().mensaje
             });
         }
     });
     
+    // Recorro todos los usuarios/dispositivos para enviarles el nuevo mensaje
     resultado.forEach(element => {
         
         if (element.key == "dispositivos") {
                     
             element.forEach(elementChild => {
                        
+                // Envia a todos los usuarios que no sean el que envio el mensaje
                 if (elementChild.val().username != usuario) {
                     
                     var registrationToken =  elementChild.key;
 
-                // Creamos el cuerpo de la notificación
-                var message = {
+                    // Creamos el cuerpo de la notificación
+                    var message = {
                     
-                    //Mensaje de la notificación
-        
-                    notification:{
-                        "title":usuario,
-                        "body": msg 
-                    },
+                        //Mensaje de la notificación
+                        notification:{
+                            "title":usuario,
+                            "body": msg 
+                        },
                 
-                    token: registrationToken
-                };
+                        token: registrationToken
+                    };
 
-                //Envío de la notificación
-                admin.messaging().send(message)
-                    .then((response) => {
-                        // Response is a message ID string.
-                        console.log('Mensaje enviado correctamente:', response);
-                    })
-                    .catch((error) => {
-                        console.log('Error enviando el mensaje:', error);
-                    });
+                    //Envío de la notificación
+                    admin.messaging().send(message)
+                        .then((response) => {
+                            // Response is a message ID string.
+                            console.log('Mensaje enviado correctamente a' + elementChild.val().username + ':', response);
+                        })
+                        .catch((error) => {
+                            console.log('Error enviando el mensaje a' + elementChild.val().username + ':', error);
+                        });
                     
                 }       
-
             });
         }
-        
-        console.log("KEY: " + element.key);
     });
     
     
